920a2402b32aabfc19f602b877f54822f7dc99ad
fminhtu <fminhtu@gmail.com>
2022-04-01 21:23:32 +0700

    setting
    - account preferences fragment
    - change password fragment

---
+++ app/src/main/java/com/example/project/ChangePasswordActivity.kt
@@ -0,0 +1,13 @@
+package com.example.project
+
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+
+class ChangePasswordActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.fragment_change_password)
+    }
+
+}
