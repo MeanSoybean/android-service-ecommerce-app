4bfcfc15e89ba4de36b0b3272f57a124fc6b5db4
MeanSoybean <bioclaw@gmail.com>
2022-04-08 22:33:31 +0700

    April 8th, 2022 Added basic sign in and sign up functionality.

---
+++ app/src/main/java/com/example/androiddevelopmentgroup7/MainActivity.kt
@@ -0,0 +1,69 @@
+package com.example.androiddevelopmentgroup7
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.Button
+import com.firebase.ui.auth.AuthUI
+import com.firebase.ui.auth.FirebaseAuthUIActivityResultContract
+import com.firebase.ui.auth.data.model.FirebaseAuthUIAuthenticationResult
+import com.google.firebase.auth.FirebaseAuth
+
+class MainActivity : AppCompatActivity() {
+    private val signInLauncher = registerForActivityResult(
+        FirebaseAuthUIActivityResultContract()
+    ) { res ->
+        this.onSignInResult(res)
+    }
+
+    private lateinit var buttonSignIn: Button
+    private lateinit var buttonSignUp: Button
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        buttonSignIn = findViewById(R.id.button_sign_in)
+        buttonSignIn.setOnClickListener { startSignInActivity() }
+
+        buttonSignUp = findViewById(R.id.button_sign_up)
+        buttonSignUp.setOnClickListener{ startSignUpActivity() }
+    }
+
+    private fun startSignUpActivity() {
+        intent = Intent(this, SignUpActivity::class.java)
+        startActivity(intent)
+    }
+
+    private fun startSignInActivity() {
+        // Choose authentication providers
+        val providers = arrayListOf(
+            AuthUI.IdpConfig.EmailBuilder().build(),
+            AuthUI.IdpConfig.PhoneBuilder().build(),
+            AuthUI.IdpConfig.GoogleBuilder().build(),
+            AuthUI.IdpConfig.FacebookBuilder().build()
+        )
+
+        // Create and launch sign-in intent
+        val signInIntent = AuthUI.getInstance()
+            .createSignInIntentBuilder()
+            .setAvailableProviders(providers)
+            .build()
+        signInLauncher.launch(signInIntent)
+    }
+
+    private fun onSignInResult(result: FirebaseAuthUIAuthenticationResult) {
+        val response = result.idpResponse
+        if (result.resultCode == RESULT_OK) {
+            // Successfully signed in
+            val user = FirebaseAuth.getInstance().currentUser
+            // ...
+
+        } else {
+            // Sign in failed. If response is null the user canceled the
+            // sign-in flow using the back button. Otherwise check
+            // response.getError().getErrorCode() and handle the error.
+            // ...
+        }
+    }
+}
